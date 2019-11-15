03c87c63428f3751be9bdd28c0732d4ff5013ff9
Kevin Perez <kevinperez996@outlook.com>
2019-11-15 00:13:24 -0500

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require ("mysql"); 
 
 var connection = mysql.createConnection ({
-    host: "localhost",
+    host: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root", 
-    password: "", 
-    database: "Burgers_db"
+    user: "cvj7n162kqpzg32l", 
+    password: "n1p03dmj2q1q9yi6", 
+    database: "aly3q24n9x2of4th"
 });
 
 connection.connect(function(err){   
