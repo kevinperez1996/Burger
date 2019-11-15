e31ff0f4518e0835525efb61412d340963eadcfe
Kevin Perez <kevinperez996@outlook.com>
2019-11-14 23:52:34 -0500

    final styling and logic

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,13 @@
 
-    
-    $(".devour-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var burger_id = $(this).children(".burger_id").val();
-      console.log(burger_id);
-      $.ajax({
-        method: "PUT",
-        url: "/burger/" + burger_id
-      }).then(function(data) {
-        // reload page to display devoured burger in proper column
-        location.reload();
-      });
-    });
+
+$(".eatItNow").on("submit", function (event) {
+  event.preventDefault();
+   
+  var burger_id = $(this).children(".burger_id").val();
+  $.ajax({
+    method: "PUT",
+    url: "/burger/" + burger_id
+  }).then(function (data) {
+    location.reload();
+  });
+});
